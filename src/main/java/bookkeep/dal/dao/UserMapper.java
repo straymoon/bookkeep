23380ddb7146dcd69cbf91b4951f5f2d737282bd
lizhe <lizhe@bell.com>
2019-02-20 10:02:58 +0800

    1

---
+++ src/main/java/bookkeep/dal/dao/UserMapper.java
@@ -0,0 +1,12 @@
+package bookkeep.dal.dao;
+
+import org.beetl.sql.core.mapper.BaseMapper;
+import org.springframework.stereotype.Repository;
+
+import bookkeep.dal.po.UserPO;
+
+
+@Repository
+public interface UserMapper extends BaseMapper<UserPO>{
+	
+}
